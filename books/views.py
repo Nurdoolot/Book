3b07154c5f75c51aad94ee35aa67c29e986cfadc
Nurdoolot <rayfi.vv@gmail.com>
2020-04-09 16:46:25 +0600

    Added system CRUD and Comment

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-
-from .models import Book
+from .forms import CommentForm
+from .models import Book, Comment
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -13,12 +13,26 @@ class HomeListView(ListView):
 
 class BookListView(ListView):
     queryset = Book.objects.order_by('-id')[:8]
-    template_name = 'book/new.html'
+    template_name = 'book/novelty.html'
     context_object_name = 'books'
 
-class BookDetailView(DetailView):
-    model = Book
-    template_name = 'book/read.html'
+
+def book_detail_view(request, pk):
+    post = get_object_or_404(Book, pk=pk)
+    comments = Comment.objects.filter(post=post).order_by('-id')
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST or None)
+        if comment_form.is_valid():
+            username = request.POST.get('username')
+            text = request.POST.get('text')
+            comment = Comment.objects.create(post=post, username=username, text=text)
+            comment.save()
+    else:
+        comment_form = CommentForm()
+
+    context = {'books': post, 'comments': comments, 'comment_form': comment_form}
+    return render(request, 'book/detail.html', context)
 
 
 class BookRandomView(ListView):
@@ -39,9 +53,11 @@ class BookUpdate(UpdateView):
     template_name = 'book/update.html'
     fields = ['title', 'book']
     success_url = reverse_lazy('home')
+    context_object_name = 'books'
 
 
 class BookDelete(DeleteView):
     model = Book
     template_name = 'book/delete.html'
     success_url = reverse_lazy('home')
+    context_object_name = 'books'
