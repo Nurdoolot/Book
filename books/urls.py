ff8f86732d85aea3b110737c5edf68f84ce1d65b
Nurdoolot <rayfi.vv@gmail.com>
2020-04-09 16:50:15 +0600

    xmm

--- books/urls.py
+++ books/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import HomeListView, BookListView, BookDetailView, BookRandomView, BookCreate, BookUpdate, BookDelete
+from .views import HomeListView, BookListView, book_detail_view, BookRandomView, BookCreate, BookUpdate, BookDelete
 
 
 urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
-    path('new/', BookListView.as_view(), name='new'),
-    path('view/<int:pk>', BookDetailView.as_view(), name='read'),
+    path('novelty/', BookListView.as_view(), name='novelty'),
+    path('detail/<int:pk>', book_detail_view, name='detail'),
     path('random/', BookRandomView.as_view(), name='random'),
     path('create/', BookCreate.as_view(), name='create'),
-    path('edit/<int:pk>', BookUpdate.as_view(), name='edit'),
+    path('update/<int:pk>', BookUpdate.as_view(), name='update'),
     path('delete/<int:pk>', BookDelete.as_view(), name='delete'),
 ]
