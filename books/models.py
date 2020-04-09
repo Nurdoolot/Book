37f3da2bab7c9c0fd8d4613e71474a8e95d08633
Nurdoolot <rayfi.vv@gmail.com>
2020-04-09 16:48:25 +0600

    Created new models for Comment

--- books/models.py
+++ books/models.py
@@ -5,7 +5,15 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=140)
     book = models.TextField()
-    image = models.ImageField(upload_to='book/')
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Book, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100)
+    text = models.TextField(max_length=150)
+
+    def __str__(self):
+        return f'{self.post.title}, {self.username}'
